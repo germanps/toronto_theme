bc4afb908e5cffeec1fc4d1931b266a01faa07f6
unknown <germanps@outlook.es>
2018-08-19 18:28:22 +0200

    Pagina especifica para el blog (WP_query)

--- functions.php
+++ functions.php
@@ -16,6 +16,7 @@
     add_theme_support('post-thumbnails');
     //tamaños imágenes
     add_image_size('destacada', 1200, 420, true);
+    add_image_size('guia-toronto', 350, 210, true);
     //agregar los widgets
     function theme_widgets(){
         register_sidebar( array(
--- page-blog.php
+++ page-blog.php
@@ -19,11 +19,46 @@
         <div class="flex-container">
             <div id="primary" class="primary">
                 
-                desde page-blog.php
-            </div>
+                <!-- Custom query de wordpress -->
+                <?php $args = array(
+                    'cat' => array(4,3,5,6),
+                    'post_per_page' => 6,
+                    'orderby' => 'date',
+                    'order' => 'DESC',
+                ); ?>
+                <?php $guiaToronto = new WP_Query($args); ?>
+
+                <?php while($guiaToronto->have_posts() ) : $guiaToronto->the_post(); ?>
+                
+                <div class="post-toronto">
+                    <div class="imagen">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail('guia-toronto') ?>
+                        </a>
+                        <div class="categoria">
+                            <?php the_category(); ?>
+                        </div>
+                    </div>
+
+                    <div class="contenido">
+                        <h3><?php the_title(); ?></h3>
+                        <?php the_excerpt(); ?>
+                        <a href="<?php the_permalink(); ?>">Leer mas..</a>
+                    </div>
+
+                    <div class="info-post">
+                        <p class="autor">Por: <?php the_author() ?></p>
+                        <p class="fecha"><?php the_date() ?></p>
+
+                    </div>
+
+                </div> <!-- End post-toronto -->
+            
+                <?php endwhile; wp_reset_postdata() ?>
+
+            </div> <!-- End primary -->
         
     <?php endwhile; ?>
-
 
             <?php get_sidebar(); ?>
 
