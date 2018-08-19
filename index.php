c99938c78b1aab676cf2c970919664ed632e45a2
unknown <germanps@outlook.es>
2018-08-19 17:43:28 +0200

    Crear pagina del blog (index.php)

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
-    echo "Desde la el index de la plantilla toronto";
-?>
+<?php get_header(); ?>
+
+    <div class="flex-container">
+
+        <div id="primary" class="primary">
+
+            <?php while(have_posts() ) : the_post(); ?>
+                <div class="entrada-blog">
+                    <h2><?php the_title(); ?></h2>
+                    <div class="contenido-entrada-blog"><?php the_content(); ?></div>
+                </div>
+            <?php endwhile ?>
+
+        </div>
+    
+
+
+        <?php get_sidebar(); ?>
+</div><!-- End flex-container -->
+
+
+<?php get_footer(); ?>
