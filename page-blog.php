b81d33bc84d089c2834e918f688f8ccfef525193
unknown <germanps@outlook.es>
2018-08-19 19:45:29 +0200

    Maquetacion blog

--- page-blog.php
+++ page-blog.php
@@ -27,35 +27,35 @@
                     'order' => 'DESC',
                 ); ?>
                 <?php $guiaToronto = new WP_Query($args); ?>
-
-                <?php while($guiaToronto->have_posts() ) : $guiaToronto->the_post(); ?>
-                
-                <div class="post-toronto">
-                    <div class="imagen">
-                        <a href="<?php the_permalink(); ?>">
-                            <?php the_post_thumbnail('guia-toronto') ?>
-                        </a>
-                        <div class="categoria">
-                            <?php the_category(); ?>
+                <div class="post-toronto-wrapper">     
+                    <?php while($guiaToronto->have_posts() ) : $guiaToronto->the_post(); ?>
+                    
+                    <div class="post-toronto">
+                        <div class="imagen">
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_post_thumbnail('guia-toronto') ?>
+                            </a>
+                            <div class="categoria">
+                                <?php the_category(); ?>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="contenido">
-                        <h3><?php the_title(); ?></h3>
-                        <?php the_excerpt(); ?>
-                        <a href="<?php the_permalink(); ?>">Leer mas..</a>
-                    </div>
+                        <div class="contenido">
+                            <h3><?php the_title(); ?></h3>
+                            <?php the_excerpt(); ?>
+                            <a href="<?php the_permalink(); ?>">Leer mas..</a>
+                        </div>
 
-                    <div class="info-post">
-                        <p class="autor">Por: <?php the_author() ?></p>
-                        <p class="fecha"><?php the_date() ?></p>
+                        <div class="info-post">
+                            <p class="autor">Por: <span><?php the_author() ?></span></p>
+                            <p class="fecha"><?php the_time(get_option('date-format') ); ?></p>
 
-                    </div>
+                        </div>
 
-                </div> <!-- End post-toronto -->
-            
-                <?php endwhile; wp_reset_postdata() ?>
-
+                    </div> <!-- End post-toronto -->
+                
+                    <?php endwhile; wp_reset_postdata() ?>
+                </div> <!-- End post-toronto-wrapper -->
             </div> <!-- End primary -->
         
     <?php endwhile; ?>
