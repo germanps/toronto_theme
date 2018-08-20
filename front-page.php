cf1ec9f32c9ac111dd1cbf1b91a8e4ca0b93a0ba
unknown <germanps@outlook.es>
2018-08-20 16:37:06 +0200

    WP_query para info y consejos en la home

--- front-page.php
+++ front-page.php
@@ -4,5 +4,30 @@
         <?php dynamic_sidebar('front-page') ?>
     </div>
     
+    <div class="informacion-consejos">
+        <div class="informacion">
+            <?php $informacion = new WP_Query('page_id=8'); ?>
+            <?php while($informacion->have_posts()) : $informacion->the_post(); ?>
+                <h3><?php the_title(); ?></h3>
+                <?php the_excerpt(); ?>
 
+            <?php endwhile; wp_reset_postdata(); ?>
+        </div>
+        <div class="consejos">
+            <h2>Consejos para viajar a Canadá</h5>
+            <?php $args = array(
+                'cat' => 11,
+                'post_per_page' => 2,
+                'order' => 'DESC',
+                'orderby' => 'date'
+            );?>
+            <?php $consejos = new WP_Query($args); ?>
+            <?php while($consejos->have_posts()) : $consejos->the_post(); ?>
+                <?php the_post_thumbnail('guia-toronto');?>
+                <h3><?php the_title() ?></h3>
+                <?php the_excerpt();?>
+            <?php endwhile; wp_reset_postdata(); ?>
+
+        </div>
+    </div>
 <?php get_footer(); ?>
